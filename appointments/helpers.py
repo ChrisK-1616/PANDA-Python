0b16379b18f9d470753e4b6040c62b36d11b02e3
Chris Knowles <chris.knowles@btinternet.com>
2023-06-13 13:58:51 +0100

    File Comments, Views and Helper Stubs Added
    Header comments for each script file added along with stubs for the required view and helper functions.

---
+++ appointments/helpers.py
@@ -0,0 +1,13 @@
+# File: helpers.py
+# Description: Various helper functions for views in the appointments app
+# Author: Chris Knowles
+# Date: Jun 2023
+
+
+# Helper functions
+def validate_nhs_number(nhs_number: str) -> bool:
+    return False
+
+
+def format_postcode(postcode: str) -> str:
+    return postcode
